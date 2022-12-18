401f82c3948e5e6ff6b85f2285d26e5b61c22eb5
Esquivelgit <esquivelnoe87@gmail.com>
2022-12-17 22:33:13 -0800

    company selection happy path

--- tests/company-selection.spec.ts
+++ tests/company-selection.spec.ts
@@ -1,3 +1,22 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("Company Selection tests", () => {});
+test.describe("Company Selection tests", () => {
+  test("Clicking Plooto Inc leads to dashboard", async ({ page }) => {
+    await page.goto("/company_select");
+    const rows = await page.locator("tr");
+    const plootoRow = await rows
+      .locator(":scope", { hasText: "Plooto Inc" })
+      .first();
+
+    await expect(await plootoRow.textContent()).toContain("Plooto Inc");
+    await expect(await plootoRow.textContent()).toContain(
+      "Subscription Active"
+    );
+
+    //click Plooto
+    await plootoRow.click();
+
+    //Validate redirection to dashbaord
+    await expect(page).toHaveURL(/.*dashboard/);
+  });
+});
