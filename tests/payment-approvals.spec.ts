3ad88677a2efadf788f39fd805d18753cafd8ff1
Esquivelgit <esquivelnoe87@gmail.com>
2022-12-18 00:32:39 -0800

    More basic validation tests

--- tests/payment-approvals.spec.ts
+++ tests/payment-approvals.spec.ts
@@ -1,3 +1,17 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("Payment Approval tests", () => {});
+test.describe("Payment Approval tests", () => {
+  test("Payment approvals validation", async ({ page }) => {
+    await page.goto("/payment_approvals");
+
+    const paymentsAwaitingApproval = await page.getByRole("tab", {
+      name: "Payments Awaiting My Approval",
+    });
+    const pendingPayables = await page.getByRole("tab", {
+      name: "All Payments Awaiting Approval",
+    });
+
+    await expect(paymentsAwaitingApproval).toBeVisible();
+    await expect(pendingPayables).toBeVisible();
+  });
+});
--- tests/pending-payments.spec.ts
+++ tests/pending-payments.spec.ts
@@ -1,3 +1,24 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("Pending Payments tests", () => {});
+test.describe("Pending Payments tests", () => {
+  test("Pending Payments validation", async ({ page }) => {
+    await page.goto("/pending_payments");
+    const rows = await page.locator("tr");
+
+    const pendingPayables = await page.getByRole("tab", {
+      name: "Pending Payables",
+    });
+    const pendingReceivables = await page.getByRole("tab", {
+      name: "Pending Receivables",
+    });
+
+    //Validate Pending Payables and Pending Receivables buttons
+
+    await expect(pendingPayables).toBeVisible();
+    await expect(pendingReceivables).toBeVisible();
+
+    //payment to Cavages links to payment approval page
+    await rows.locator("text=Cavages").click();
+    await expect(page).toHaveURL(/.*payment_approval/);
+  });
+});
